57f43364326538d9531f6a4b13cebb9577871fef
gabrielolivra <gabernardinooliveira@gmail.com>
2025-06-04 22:16:14 -0300

    feat: add bearer authentication to Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
     .setDescription('API desenvolvida para encurtar links')
     .setVersion('1.0')
     .addTag('teddy')
+    .addBearerAuth()
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
